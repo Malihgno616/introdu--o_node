b6dc7c38bcd0ac8ffd2844171be6e7b3a96368a1
ackindev <ackinlino123@gmail.com>
2024-11-29 08:43:41 -0300

    Node, IO, console e variáveis de ambiente

--- arquivos.js
+++ arquivos.js
@@ -1,16 +1,21 @@
 const path = require("node:path");
 const fs = require("node:fs");
+const dotenv = require("dotenv");
+dotenv.config();
+console.log(process.env.NODE_ENV);
 
 const filePath = path.join(process.cwd(), "texto.txt");
 const fileOutPath = path.join(process.cwd(), "texto-com-linhas.txt");
 console.log(filePath);
 
-
+console.time("leitura do arquivo"); // Inicie o temporizador para leitura do arquivo
 fs.readFile(filePath, {}, (error, dados) => {
   if (error) {
     console.error(`Erro na leitura do arquivo no caminho ${filePath}`);
     return;
   }
+
+  console.timeEnd("leitura do arquivo"); // Encerre o temporizador para leitura do arquivo
 
   const texto = dados.toString();
   const linhas = texto.split("\n");
@@ -19,11 +24,13 @@ fs.readFile(filePath, {}, (error, dados) => {
     (linha, index) => `${index + 1} - ${linha}`
   );
 
+  console.time("escrita do arquivo"); // Inicie o temporizador para a escrita do arquivo
   fs.writeFile(fileOutPath, linhasAjustadas.join("\n"), {}, (erro) => {
     if (erro) {
       console.log(`Erro na escrita do arquivo no caminho ${fileOutPath}`);
     } else {
-      console.log(`Arquivo escrito com sucesso em ${fileOutPath}`);
+      console.log(`Arquivo salvo no bucket ${process.env.S3_BUCKET}`);
+      console.timeEnd("escrita do arquivo"); // Encerre o temporizador para a escrita do arquivo
     }
   });
 });
